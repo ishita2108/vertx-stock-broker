a05bd3dadb69739f4b44749c560f34e0a5e7d76e
ishita2108 <iwinner21@gmail.com>
2025-01-20 15:38:39 +0530

    get assets api

--- src/main/java/com/web/vertx_stock_broker/MainVerticle.java
+++ src/main/java/com/web/vertx_stock_broker/MainVerticle.java
@@ -2,19 +2,50 @@ package com.web.vertx_stock_broker;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.Router;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.web.vertx_stock_broker.assets.*;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static final Logger LOG = LoggerFactory.getLogger(MainVerticle.class);
+
+  public static void main(String[] args) {
+    var vertx = Vertx.vertx();
+    vertx.exceptionHandler( error ->{
+      LOG.error("Unhandled: {} ", error);
+    });
+    vertx.deployVerticle(new MainVerticle(), ar ->{
+      if (ar.failed()) {
+        LOG.error("Failed to deploy: {} ", ar.cause());
+        return;
+      }
+      LOG.info("Deployed Main Verticle {} ", MainVerticle.class.getName());
+    });
+  }
+
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
-    vertx.createHttpServer().requestHandler(req -> {
-      req.response()
-        .putHeader("content-type", "text/plain")
-        .end("Hello from Vert.x!");
-    }).listen(8888).onComplete(http -> {
+
+    final Router restApi = Router.router(vertx);
+    AssetsRestApi.attach(restApi);
+    vertx.createHttpServer().requestHandler(restApi
+//      req -> {
+//      req.response()
+//        .putHeader("content-type", "text/plain")
+//        .end("Hello from Vert.x!");
+//    }
+    ).exceptionHandler(error ->{
+      LOG.error("HTTP Server Error {} ",error);
+      })
+      .listen(8888).onComplete(http -> {
       if (http.succeeded()) {
         startPromise.complete();
-        System.out.println("HTTP server started on port 8888");
+        LOG.info("HTTP server started on port 8888");
       } else {
         startPromise.fail(http.cause());
       }
